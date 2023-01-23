a2e2ad4adc1a24fe3f55370e164d9339df1ce5ab
Igor <igor.balakirev@mail.ru>
2023-01-23 18:24:29 +0300

    Entity Framework

--- RingPipeline/Program.cs
+++ RingPipeline/Program.cs
@@ -2,9 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
+using Microsoft.Data.Sqlite.Properties;
+
 
 namespace RingPipeLine
 {
+    
+    class SQLiteConnection
+    {
+        public string connectionString = "Data Source=hello.db";
+        public string connectionStringExt = "Data Source=usersdata3.db;Cache=Shared;Mode=ReadOnly;";
+
+        public bool CreateConnection()
+        {
+            //SQLiteConnection
+
+            //SQLitePCL
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                
+
+                connection.Open();
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+    
+    
     static class Program
     {
         public static FormMain formMain;
@@ -16,6 +45,9 @@ namespace RingPipeLine
         [STAThread]
         static void Main()
         {
+            bool con = new SQLiteConnection().CreateConnection();
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             formMain = new FormMain();
